62a42290cc5ee2542058411fa425cd095f6fd53b
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-09-06 08:07:59 +0300

    update landing page

--- js/scripts.js
+++ js/scripts.js
@@ -140,3 +140,10 @@ $(document).ready(function(){
     $("#para4").toggle();
   });
 });
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var name=$("input#name").val();
+  alert("Hello "+name+" Thank you for contacting us");
+  event.preventDefault();
+  });
+});
